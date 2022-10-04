2c501fdf0896d2ca0d4acb280d00d0985c6a2619
aiyayayaya <harmonyyap@gmail.com>
2022-10-04 21:57:22 +0100

    112 done

---
+++ leetcode/112-PathSum.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root){
+            return false;
+        }
+        bool leftres = false, rightres = false;
+        if (!root->left && !root->right){
+            return root->val == targetSum;
+        }
+        if (root->left){
+            leftres = hasPathSum(root->left, targetSum - root->val);
+        }
+        if (root->right){
+            rightres = hasPathSum(root->right, targetSum - root->val);
+        }
+        return leftres || rightres;
+    }
+};
