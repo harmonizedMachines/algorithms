b1c41c7820d954e03a78ab850e24039d27272d2d
aiyayayaya <harmonyyap@gmail.com>
2022-11-23 22:52:47 +0000

    daily

---
+++ leetcode/36-ValidSudoku.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool checkRow[9][9] = {false};
+        bool checkCol[9][9] = {false};
+        bool checkBox[9][9] = {false};
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                if (board[i][j] != '.'){
+                    int num = board[i][j] - '0' - 1;
+                    int k = i / 3 * 3 + j / 3;
+                    if (checkRow[i][num] || checkCol[j][num] || checkBox[k][num]){
+                        return false;
+                    }
+                    checkRow[i][num] = checkCol[j][num] = checkBox[k][num] = true;
+                }
+            }
+        }
+        return true;
+    }
+};
