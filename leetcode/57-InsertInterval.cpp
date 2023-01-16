458d12cc569e718801b6dd15867f91ef2bbaa1d1
aiyayayaya <harmonyyap@gmail.com>
2023-01-16 22:49:53 +0000

    daily

---
+++ leetcode/57-InsertInterval.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        int i = 0;
+        for (auto&& i: intervals){
+            if (i[1] < newInterval[0]){
+                ans.push_back(i);
+            } else if (newInterval[1] < i[0]){
+                ans.push_back(newInterval);
+                newInterval = i;
+            } else {
+                newInterval[0] = min(i[0], newInterval[0]);
+                newInterval[1] = max(i[1], newInterval[1]);
+            }
+        }
+        ans.push_back(newInterval);
+        return ans;
+    }
+};
