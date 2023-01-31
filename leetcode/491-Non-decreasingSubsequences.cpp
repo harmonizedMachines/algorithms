ee6078b84156fac6ae843552382587315d0f30cb
aiyayayaya <harmonyyap@gmail.com>
2023-01-31 22:33:41 +0000

    491

--- leetcode/491-Non-decreasingSubsequences.cpp
+++ leetcode/491-Non-decreasingSubsequences.cpp
@@ -1,22 +1,23 @@
 class Solution {
-public:
-    vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> seq;
-        dfs(res, seq, nums, 0);
-        return res;
-    }
-    
-    void dfs(vector<vector<int>>& res, vector<int>& seq, vector<int>& nums, int pos) {
-        if(seq.size() > 1) res.push_back(seq);
+    vector<vector<int>> ans;
+    void dfs(vector<int>& seq, vector<int>& nums, int i){
+        if (seq.size() > 1){
+            ans.push_back(seq);
+        }
         unordered_set<int> hash;
-        for(int i = pos; i < nums.size(); ++i) {
-            if((seq.empty() || nums[i] >= seq.back()) && hash.find(nums[i]) == hash.end()) {
-                seq.push_back(nums[i]);
-                dfs(res, seq, nums, i + 1);
+        for (int p = i; p < nums.size(); ++p){
+            if ((seq.size() == 0 || nums[p] >= seq.back()) && hash.find(nums[p]) == hash.end()){
+                seq.push_back(nums[p]);
+                dfs(seq, nums, p + 1);
                 seq.pop_back();
-                hash.insert(nums[i]);
+                hash.insert(nums[p]);
             }
         }
+    }
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<int> seq;
+        dfs(seq, nums, 0);
+        return ans;
     }
 };
